993e62d7bbefd3083a2cd1034c1ef4256a7244f4
johnmarkuscs <markuscabrera.cs@gmail.com>
2023-04-04 15:03:11 -0400

    Small change

--- src/middleware/validateResource.ts
+++ src/middleware/validateResource.ts
@@ -2,11 +2,11 @@ import { Request, Response, NextFunction } from 'express';
 import { AnyZodObject } from 'zod';
 
 const validate = 
-	(schema: AnyZodObject) => 
+	(schemaUser: AnyZodObject) => 
 	(req: Request, res: Response, next: NextFunction) => {
 
 		try {
-			schema.parse({
+			schemaUser.parse({
 				body: req.body,
 				query: req.query,
 				params: req.params
